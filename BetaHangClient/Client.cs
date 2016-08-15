35346a89f7801cbd3b9528b59908a12bbafc8da1
SamuelWr <samuel.wraak@gmail.com>
2016-08-15 16:33:03 +0200

    Null exception on exit

--- BetaHangClient/Client.cs
+++ BetaHangClient/Client.cs
@@ -33,7 +33,7 @@ namespace BetaHangClient
         public void RequestShutdown()
         {
             shutdown = true;
-            client.Close();
+            client?.Close();
             listenerThread = null;
             client = null;
 
